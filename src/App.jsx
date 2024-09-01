29d20e1bb4fc1c78d882784affd53f66d37b8fa1
S7uy0g <RaiJoker103@gmail.com>
2024-09-01 14:04:06 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SearchPokemon from './Search/search'
+
+function App() {
+
+  return (
+    <>
+      <SearchPokemon/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Search/search.jsx
@@ -0,0 +1,39 @@
+import './style.css'; 
+
+function findPokemon(){
+    var pokeName=document.getElementById("searchBox").value.toLowerCase();
+    findInAPI(pokeName);
+}
+
+async function findInAPI(pokeName) {
+    try {
+        const response=await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+        if(!response.ok){
+            throw new Error("Could not find data");
+        }
+        const values=await response.json();
+        const image=document.getElementById("pokemonImage");
+        image.src=values.sprites.front_default;
+        image.classList.add("display");
+
+    } catch (error) {
+        document.getElementById("error").innerHTML=error;
+    }
+}
+
+function SearchPokemon(){
+    return (
+        <>
+            <div className='search'>
+                <input type="text" id="searchBox" className="searchBox" placeholder="Input Pokemon Name"/>
+                <button id="searchButton" className="searchButton" onClick={findPokemon}>&#128269;</button>
+            </div>
+            <div className='answer'>
+                <p id="error"></p>
+                <img src="" alt="Pokemon Image" id="pokemonImage" className="pokemonImage"/>
+            </div>
+        </>
+    );
+}
+
+export default SearchPokemon
