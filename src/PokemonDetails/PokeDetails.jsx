e8bebcc3da54990bebfe8ab1a376ff65b3ab6d03
S7uy0g <RaiJoker103@gmail.com>
2024-09-08 14:53:50 +0545

    minor changes

--- src/PokemonDetails/PokeDetails.jsx
+++ src/PokemonDetails/PokeDetails.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 function PokemonDetails() {
-    const { name } = useParams(); // Retrieve the Pokemon name from the URL
+    const { name } = useParams();
 
     return (
         <div>
             <h1>{name.toUpperCase()} Details</h1>
-            {/* Add your own content here */}
         </div>
     );
 }
